6e5107b7a79ad949277cd7e3a16ef9dcbed848b0
adit <aditjain1980@gmail.com>
2020-05-10 11:58:00 +0530

    Updates

---
+++ precache-manifest.ba8248d306d854cd47e5f749df7c7058.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82ac3bef831f4e2cdd7dba0e2c3d3de4",
+    "url": "/bridge/index.html"
+  },
+  {
+    "revision": "fbadb79f2fb0d3b94e9b",
+    "url": "/bridge/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b22ea10d336e98fb8938",
+    "url": "/bridge/static/js/2.b33cc375.chunk.js"
+  },
+  {
+    "revision": "581070db7478fba8a52f7e9e4c335e11",
+    "url": "/bridge/static/js/2.b33cc375.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbadb79f2fb0d3b94e9b",
+    "url": "/bridge/static/js/main.b53d7a5e.chunk.js"
+  },
+  {
+    "revision": "77c24a589b6d8417c6d0",
+    "url": "/bridge/static/js/runtime-main.fcb14e03.js"
+  }
+]);
